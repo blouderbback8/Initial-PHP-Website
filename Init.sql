756905ddec30e2bccf000e701b11248bed269892
blouderbback8 <brianlouderback@student.olympic.edu>
2024-10-16 20:39:41 -0700

    Update Init.sql

--- Init.sql
+++ Init.sql
@@ -8,7 +8,8 @@ USE bjj_lineage;
 -- Create Users table
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     password VARCHAR(100) NOT NULL
 );
 
